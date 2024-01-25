d9e4c695bbbf67b6cbde94be04ea05b2787b8eeb
sstreichan <150350058+sstreichan@users.noreply.github.com>
2024-01-25 11:44:23 +0100

    docstrings update

--- util.py
+++ util.py
@@ -21,36 +21,64 @@ def get_data_folder():
 
 
 def get_rnd_datetime():
-    _dateTime = datetime(2024, 10, 31, random.randint(0, 23), random.randint(0, 59), random.randint(0, 59))
-    return _dateTime
+    """
+    Generiert und gibt eine zufällige Datetime-Instanz zurück.
 
-    random_time = f"{hour:02d}:{minute:02d}:{second:02d}"
-    start_date = datetime(2024, 1, 1)  # Startdatum
-    end_date = datetime(2024, 12, 31)  # Enddatum
-    random_days = random.randint(0, (end_date - start_date).days)
-    random_date = start_date + timedelta(days=random_days)
-    return f"{random_date}{random_time}"
-
+    Returns:
+    datetime: Eine zufällige Datetime-Instanz im Jahr 2024, im Zeitraum von 0 bis 23 Stunden,
+             0 bis 59 Minuten und 0 bis 59 Sekunden.
+    """    
+    return  datetime(2024, 10, 31, random.randint(0, 23), random.randint(0, 59), random.randint(0, 59))
 
 def get_rnd_time():
-    _dateTime = f"{random.randint(0, 23)}:{random.randint(0, 59)}"
-    return _dateTime
+    """
+    Generiert und gibt eine zufällige Uhrzeit im Format HH:MM zurück.
+
+    Returns:
+    str: Eine zufällige Uhrzeit im Format HH:MM.
+    """
+    return f"{random.randint(0, 23)}:{random.randint(0, 59)}"
 
 def get_rnd_fluggesellschaft():
-    with open(f"{util.get_data_folder()}/data/randomData.json", "r", encoding="utf8") as f:
+    """
+    Gibt zufällige Fluggesellschaftsdaten zurück.
+
+    Returns:
+    tuple: Ein Tupel bestehend aus dem Namen der Fluggesellschaft und einer zufälligen Kennung.
+    """
+    with open(f"{get_data_folder()}/data/randomData.json", "r", encoding="utf8") as f:
         data = json.loads(f.read())
         fluggesellschaft = random.choice(data["fluggesellschaften"])
         return fluggesellschaft["name"], f"{fluggesellschaft['kennung']}{random.randint(1000, 9999)}"
 
+
 def get_rnd_ziel():
-    with open(f"{util.get_data_folder()}/data/randomData.json", "r", encoding="utf8") as f:
+    """
+    Gibt ein zufälliges Flugziel zurück.
+
+    Returns:
+    str: Das zufällige Flugziel.
+    """
+    with open(f"{get_data_folder()}/data/randomData.json", "r", encoding="utf8") as f:
         data = json.loads(f.read())
         flugziel = random.choice(data["flugziele"])
         return flugziel["ziel"]
         
 def get_rnd_gate():
+    """
+    Gibt ein zufälliges Gate zurück.
+
+    Returns:
+    str: Der Name eines zufälligen Gates.
+    """
     return f"Gate {random.randint(1, 10)}"
 
 def get_rnd_Status():
+    """
+    Gibt einen zufälligen Flugstatus zurück.
+
+    Returns:
+    str: Ein zufälliger Flugstatus (Normal, Verspätet oder Fällt aus) basierend auf Gewichtungen.
+    """
     status = [["Normal", 0.5], ["Verspätet", 0.2], ["Fällt aus", 0.1]]
     return f"{random.choices([i[0] for i in status], weights = [i[1] for i in status])[0]}"
